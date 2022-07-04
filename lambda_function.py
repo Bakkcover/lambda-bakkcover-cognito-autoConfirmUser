e90df7f2bb09f06531da6026652af5a66021785f
James Yeap <james.yeap@accenture.com>
2022-07-04 10:32:46 +0800

    Add buildspec.yml for auto-deployment to AWS Lambda

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,8 @@ def lambda_handler(event, context):
     # sets the user pool autoConfirmUser flag to true
     # without any validation
     event['response']['autoConfirmUser'] = True
-    
+
+    # test if auto-deploy works
     print(event)
 
     # Return to Amazon Cognito
